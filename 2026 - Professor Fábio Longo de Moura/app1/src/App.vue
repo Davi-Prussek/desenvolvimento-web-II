e6457c633dc3651af95d6ac05490f0add1ec3c02
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-02-23 11:56:24 -0300

    FEAT: Atividade Vecel

---
+++ 2026 - Professor Fábio Longo de Moura/app1/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, } from 'vue';
+
+let contador = ref(0)
+
+function manipular(event) {
+if (event.target.id == 'adicao1') {
+  contador.value++;
+}
+else if (event.target.id == 'adicao5') {
+  contador.value+=5;
+}
+
+else if (event.target.id == 'subtracao1') {
+    if (contador.value > 0) {
+      contador.value--;
+    }
+  }
+else if (event.target.id == 'subtracao5') {
+    if (contador.value > 0) {
+      contador.value-=5;
+    }
+  }
+}
+
+
+</script>
+
+<template>
+  <button  @click="manipular" id="adicao1">adicionar</button>
+  <button  @click="manipular" id="subtracao1">diminuir</button>
+  <button  @click="manipular" id="adicao5">adicionar 5</button>
+  <button  @click="manipular" id="subtracao5">diminuir 5</button>
+  <p>
+    {{
+      contador
+    }}
+  </p>
+</template>
+
+<style scoped></style>
