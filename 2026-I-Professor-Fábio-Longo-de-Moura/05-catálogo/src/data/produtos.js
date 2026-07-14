14426fa2f4772c27915da75bb05240594b157f5d
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-14 08:57:38 -0300

    FEAT: Correção e adaptação de nome de pastas, criação de app vue para atividade coffee

---
+++ 2026-I-Professor-Fábio-Longo-de-Moura/05-catálogo/src/data/produtos.js
@@ -0,0 +1,13 @@
+const produtos = [
+{ id: 1, nome: 'Ração Premium Cães', preco: 120, categoria: 'Alimentos', imagem: '/images/filhotes-pp-20-kg-qdfzlq.webp' },
+{ id: 2, nome: 'Ração Gatos Castrados', preco: 95, categoria: 'Alimentos', imagem: '/images/filhotes-pp-20-kg-qdfzlq.webp' },
+{ id: 3, nome: 'Petisco Natural', preco: 18, categoria: 'Alimentos', imagem: '/images/filhotes-pp-20-kg-qdfzlq.webp' },
+{ id: 4, nome: 'Brinquedo Bola', preco: 22, categoria: 'Brinquedos', imagem: '/' },
+{ id: 5, nome: 'Mordedor de Corda', preco: 30, categoria: 'Brinquedos', imagem: '/' },
+{ id: 6, nome: 'Shampoo Pet', preco: 35, categoria: 'Higiene', imagem: '/images/kit-higiene-pet.webp' },
+{ id: 7, nome: 'Tapete Higiênico', preco: 42, categoria: 'Higiene', imagem: '/images/kit-higiene-pet.webp' },
+{ id: 8, nome: 'Coleira Azul', preco: 28, categoria: 'Acessórios', imagem: '/images/acessorio.jpg' },
+{ id: 9, nome: 'Guia de Passeio', preco: 40, categoria: 'Acessórios', imagem: '/images/acessorio.jpg' }
+]
+
+export { produtos as listaProdutos }
