074a542e7ea2f3afd13b6540c62bf3f918aa01a3
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-16 22:18:01 -0300

    FEAT: Configuração de ambiente, de pastas, início de criação de views e componentes

---
+++ 2026-II-Professor-Cristofer-Sousa/Atividade-Coffee/src/components/CardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <img :src="'../../public/icons/'+props.img.src" :alt="props.img.alt">
+  <div>
+    <h3>{{ props.titulo }}</h3>
+    <p>{{ props.valor }}</p>
+    <p>{{ props.desc }}</p>
+    <p>{{ props.descsecundaria }}</p>
+  </div>
+</div>
+</template>
+<script setup lang="ts">
+type notaTipos = string | number
+
+interface imagem {
+  src: string
+  alt: string
+}
+const props = defineProps<{
+  img: imagem
+  titulo: string
+  valor: notaTipos
+  desc?: string
+  descsecundaria?: string
+}>()
+</script>
--- 2026-II-Professor-Cristofer-Sousa/Atividade-Coffee/vite.config.ts
+++ 2026-II-Professor-Cristofer-Sousa/Atividade-Coffee/vite.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+
   ],
   resolve: {
     alias: {
